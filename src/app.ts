35a8d70a4868c10a0c8cbdd3fbeb36271e489aba
Francisco Sierra <64598498+franciscosierra1915@users.noreply.github.com>
2022-04-12 15:54:12 -0500

    car position moved

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,13 @@
 import "@babylonjs/core/Debug/debugLayer";
 import "@babylonjs/inspector";
 import "@babylonjs/loaders/glTF";
-import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder, FreeCamera, Color4 } from "@babylonjs/core";
-import { AdvancedDynamicTexture, StackPanel, Button, TextBlock, Rectangle, Control, Image } from "@babylonjs/gui";
-
-//enum for states
-enum State { START = 0, GAME = 1, LOSE = 2, CUTSCENE = 3 }
+import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder, StandardMaterial, Color3, Texture, Vector4, SceneLoader } from "@babylonjs/core";
 
 class App {
     // General Entire Application
     private _scene: Scene;
     private _canvas: HTMLCanvasElement;
     private _engine: Engine;
-
-    //Scene - related
-    private _state: number = 0;
 
     constructor() {
         this._canvas = this._createCanvas();
@@ -23,10 +16,74 @@ class App {
         this._engine = new Engine(this._canvas, true);
         this._scene = new Scene(this._engine);
 
-        var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), this._scene);
+        var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2.5, 10, Vector3.Zero(), this._scene);
         camera.attachControl(this._canvas, true);
         var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), this._scene);
-        var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, this._scene);
+
+        // var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, this._scene);
+
+        /**** Materials *****/
+        //color
+        const groundMat = new StandardMaterial("groundMat", this._scene);
+        groundMat.diffuseColor = new Color3(0, 1, 0);
+
+        //texture
+        const roofMat = new StandardMaterial("roofMat", this._scene);
+        roofMat.diffuseTexture = new Texture(
+            "https://assets.babylonjs.com/environments/roof.jpg", this._scene
+        );
+        const boxMat = new StandardMaterial("boxMat", this._scene);
+        boxMat.diffuseTexture = new Texture(
+            "https://assets.babylonjs.com/environments/cubehouse.png", this._scene
+        );
+
+        //options parameter to set different images on each side
+        const faceUV = [];
+        faceUV[0] = new Vector4(0.5, 0.0, 0.75, 1.0); //rear face
+        faceUV[1] = new Vector4(0.0, 0.0, 0.25, 1.0); //front face
+        faceUV[2] = new Vector4(0.25, 0, 0.5, 1.0); //right side
+        faceUV[3] = new Vector4(0.75, 0, 1.0, 1.0); //left side
+        // top 4 and bottom 5 not seen so not set
+
+
+        /**** World Objects *****/
+        const box = MeshBuilder.CreateBox("box", {
+            faceUV: faceUV,
+            wrap: true,
+
+        }, this._scene);
+        box.material = boxMat;
+        box.position.y = 0.5;
+        const roof = MeshBuilder.CreateCylinder("roof", {
+            diameter: 1.3,
+            height: 1.2,
+            tessellation: 3,
+        }, this._scene);
+        roof.material = roofMat;
+        roof.scaling.x = 0.75;
+        roof.rotation.z = Math.PI / 2;
+        roof.position.y = 1.22;
+        const ground = MeshBuilder.CreateGround("ground", {
+            width: 10,
+            height: 10,
+        }, this._scene);
+        ground.material = groundMat;
+
+        let car;
+
+        SceneLoader.ImportMeshAsync("", "https://assets.babylonjs.com/meshes/", "car.babylon").then(() =>  {
+            const wheelRB = this._scene.getMeshByName("wheelRB");
+            const wheelRF = this._scene.getMeshByName("wheelRF");
+            const wheelLB = this._scene.getMeshByName("wheelLB");
+            const wheelLF = this._scene.getMeshByName("wheelLF");
+            this._scene.beginAnimation(wheelRB, 0, 30, true);
+            this._scene.beginAnimation(wheelRF, 0, 30, true);
+            this._scene.beginAnimation(wheelLB, 0, 30, true);
+            this._scene.beginAnimation(wheelLF, 0, 30, true);
+            car = this._scene.getMeshByName("car");
+            car.position.x = 2;
+            car.position.y = 0.19;
+          });
 
         // hide/show the Inspector
         window.addEventListener("keydown", (ev) => {
@@ -59,52 +116,5 @@ class App {
         return this._canvas;
     }
 
-    //GoTo Functions
-    // goToStart
-    private async _goToStart() {
-        this._engine.displayLoadingUI(); //make sure to wait for start to load
-
-        //--SCENE SETUP--
-        //dont detect any inputs from this ui while the game is loading
-        this._scene.detachControl();
-        let scene = new Scene(this._engine);
-        scene.clearColor = new Color4(0, 0, 0, 1);
-        //creates and positions a free camera
-        let camera = new FreeCamera("camera1", new Vector3(0, 0, 0), scene);
-        camera.setTarget(Vector3.Zero()); //targets the camera to scene origin
-
-        //...do gui related stuff
-        //create a fullscreen ui for all of our GUI elements
-        const guiMenu = AdvancedDynamicTexture.CreateFullscreenUI("UI");
-        guiMenu.idealHeight = 720; //fit our fullscreen ui to this height
-
-        //create a simple button
-        const startBtn = Button.CreateSimpleButton("start", "PLAY");
-        startBtn.width = 0.2;
-        startBtn.height = "40px";
-        startBtn.color = "white";
-        startBtn.top = "-14px";
-        startBtn.thickness = 0;
-        startBtn.verticalAlignment = Control.VERTICAL_ALIGNMENT_BOTTOM;
-        guiMenu.addControl(startBtn);
-
-        //this handles interactions with the start button attached to the scene
-        startBtn.onPointerDownObservable.add(() => {
-            this._goToCutScene();
-            scene.detachControl(); //observables disabled
-        });
-
-        //--SCENE FINISHED LOADING--
-        await scene.whenReadyAsync();
-        this._engine.hideLoadingUI(); //when the scene is ready, hide loading
-        //lastly set the current state to the start state and set the scene to the start scene
-        this._scene.dispose();
-        this._scene = scene;
-        this._state = State.START;
-    }
-
-    private async _goToCutScene(): Promise<void>{
-        return null;
-    }
 }
 new App();
